756eadcce993ebe609f523ef725664ce1fe10d04
James Fogarty <jayfo@users.noreply.github.com>
2025-12-29 11:52:01 -0800

    Comment for Future

--- src/data/CourseStoreData.ts
+++ src/data/CourseStoreData.ts
@@ -1,5 +1,6 @@
 import { CourseStoreData } from "@/types/CourseStore";
 
 // Currently unused.
+// When I start looking at this, probably look at zustand instead of mobx.
 export const courseStoreData: CourseStoreData = {
 };
